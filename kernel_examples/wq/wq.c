1decb33c86be4c87eb1422e84e878df76b7d97ef
Erno Hentonen <hentonen@gmail.com>
2018-08-07 11:51:15 +0300

    fixes

--- kernel_examples/wq/wq.c
+++ kernel_examples/wq/wq.c
@@ -1,8 +1,10 @@
-#include <linux/kernel.h> 
+#include <linux/init.h>
 #include <linux/module.h>
 #include <linux/workqueue.h>
 #include <linux/sched.h>
 #include <linux/slab.h>
+#include <linux/kernel.h>
+
 
 static struct workqueue_struct *my_wq;
 
@@ -16,49 +18,53 @@ struct my_delayed_work {
 	int data;
 };
 
+struct my_work *work;
+struct my_delayed_work *delayed_work;
+
 void workqueue_work_function(struct work_struct *work) {
-	struct my_work *my_work = (struct my_work *)work; 
-	printk(KERN_DEBUG "My work data:%d\n",my_work->data);	
-	printk(KERN_DEBUG "Ticks:%ld\n",jiffies);
-	kfree(work);
+	struct my_work *my_work = (struct my_work *)work;
+	printk(KERN_DEBUG "Working work: data: %d\n",my_work->data);
+	printk(KERN_DEBUG "Jiffies: %ld\n",jiffies);
+
 }
 
-void workqueue_delayed_work_function(struct delayed_work *delayed_work) {
-	struct my_delayed_work *my_delayed_work = (struct my_delayed_work *)delayed_work;
-	printk(KERN_DEBUG "My delayed work data:%d\n",my_delayed_work->data);
-	printk(KERN_DEBUG "Ticks:%ld\n",jiffies);
-	kfree(delayed_work);
+void workqueue_delayed_work_function(struct work_struct *work) {
+	struct my_delayed_work *my_work = (struct my_delayed_work *)work;
+	printk(KERN_DEBUG "Delayed work: data: %d\n",my_work->data);
+	printk(KERN_DEBUG "Jiffies: %ld\n",jiffies);
 }
 
-int init_my_workqueue_module(void) {
+static int __init init_workqueue_module(void) {
 	int ret;
-	struct my_work *work;
-	struct my_delayed_work *delayed_work;
-	my_wq = alloc_workqueue("my_queue",0,16);
+
+	
+	printk(KERN_DEBUG "Init workqueue_module\n");
+	my_wq = alloc_workqueue("my_queue",0,4);
 	if(my_wq) {
-		work = (struct my_work *)kmalloc(sizeof(struct my_work),GFP_KERNEL);
-		if (work) {
-			INIT_WORK((struct work_struct *)work, workqueue_work_function);
+		work = (struct my_work *)kzalloc(sizeof(struct my_work),GFP_KERNEL);
+		if(work) {
+			INIT_WORK((struct work_struct *)work,&workqueue_work_function);
 			work->data = 100;
 			ret = queue_work(my_wq, (struct work_struct *)work);
-			printk(KERN_DEBUG "Initialized work at %ld\n",jiffies);
+			printk(KERN_DEBUG "Submitted work at %ld jiffies\n",jiffies);	
 		}
-		delayed_work = (struct my_delayed_work *)kmalloc(sizeof(struct my_delayed_work),GFP_KERNEL);
+		delayed_work = (struct my_delayed_work *)kzalloc(sizeof(struct my_delayed_work),GFP_KERNEL);
 		if(delayed_work) {
-			INIT_DELAYED_WORK((struct delayed_work *)delayed_work, workqueue_delayed_work_function);
-			delayed_work->data = 200;
-			ret = queue_delayed_work(my_wq,(struct delayed_work *)delayed_work,321);
-			printk(KERN_DEBUG "Initialized delayed work at %ld\n",jiffies);
+			INIT_DELAYED_WORK((struct delayed_work *)delayed_work, &workqueue_delayed_work_function);
+			delayed_work->data=200;
+			ret = queue_delayed_work(my_wq, (struct delayed_work *)delayed_work,321);
+			printk(KERN_DEBUG "Submitted delayed work at %ld jiffies\n",jiffies);
 		}
 	}
 	return 0;
 }
 
-void clean_module(void) {
+void my_cleanup_module(void) {
 	flush_workqueue(my_wq);
 	destroy_workqueue(my_wq);
+	kfree(work);
+	kfree(delayed_work);
 }
-
-module_init(init_my_workqueue_module);
-module_exit(clean_module);
 MODULE_LICENSE("GPL");
+module_init(init_workqueue_module);
+module_exit(my_cleanup_module);
